223d22ff59cad2fe7be8c95ff68bc424c81e35b1
waterfuried <cisternsystem@gmail.com>
2022-04-04 17:05:51 +0300

    новый учебный проект: гонки. реализация

--- src/main/java/Stage.java
+++ src/main/java/Stage.java
@@ -1,6 +1,7 @@
 public abstract class Stage {
     protected int length;
     protected String description;
+    protected int restriction;
 
     public String getDescription() { return description; }
     public abstract void go(Car c);
